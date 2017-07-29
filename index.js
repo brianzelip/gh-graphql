00d6442a2f1e6f5de22ee2c3afd7e3629a82258b
Brian Zelip <bzelip@gmail.com>
2017-07-29 15:40:54 -0400

    got query working, thanks !SO!

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const fs = require('fs');
 const got = require('got');
 var CronJob = require('cron').CronJob;
 
-
-
 curl -H "Authorization: bearer token" -X POST -d " \
  { \
-   \"query\": \"query { viewer { login }}\" \
+   \"query\": \"query {organization(login: \\\"NCBI-Hackathons\\\") {login repositories(first: 100) {totalCount nodes {name license description url updatedAt languages(first: 1) {edges {node {name}}}releases(first: 1) {totalCount}}}}}\"
  } \
-" https:\/\/api.github.com\/graphql
+" https:\/\/api.github.com\/graphql > data.json
