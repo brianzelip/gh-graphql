8573f55b0974556e794e4ab7edfebd8ba7aff692
Brian Zelip <bzelip@gmail.com>
2017-07-30 12:26:49 -0400

    query GH api v4 from node and write response data to file

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
 const fs = require('fs');
-const got = require('got');
-var CronJob = require('cron').CronJob;
+const request = require('request');
 
-curl -H "Authorization: bearer token" -X POST -d " \
- { \
-   \"query\": \"query {organization(login: \\\"NCBI-Hackathons\\\") {login repositories(first: 100) {totalCount nodes {name license description url updatedAt languages(first: 1) {edges {node {name}}}releases(first: 1) {totalCount}}}}}\"
- } \
-" https:\/\/api.github.com\/graphql > data.json
+// curl -H "Authorization: bearer 159432c7e188822c3440f09b714e0e3c75839ac7" -X POST -d " \
+//  { \
+//    \"query\": \"query {organization(login: \\\"NCBI-Hackathons\\\") {login repositories(first: 100) {totalCount nodes {name description url languages(first: 1) {edges {node {name}}}releases(first: 1) {totalCount}}}}}\"
+//  } \
+// " https:\/\/api.github.com\/graphql > data.json
+
+
+// the Request docs seem to conflate Authorization and Header, although technically Authentication is a request header -- does this fact mean that they are peers, not parent-child?
+
+
+const options = {
+  'uri': 'https://api.github.com/graphql',
+  'method': 'POST',
+  'headers': {
+    'User-Agent': 'brianzelip'
+  },
+  'auth': {
+    'bearer': 'token'
+  },
+  'body': JSON.stringify({
+    "query": "query { \
+      organization(login: \"NCBI-Hackathons\") { \
+        login \
+        repositories(first: 100) { \
+          totalCount \
+          nodes { \
+            name \
+            description \
+            url \
+            languages(first: 1) { \
+              edges { \
+                node { \
+                  name \
+                } \
+              } \
+            } \
+            releases(first: 1) { \
+              totalCount \
+            } \
+            issues(first: 100, labels: \"manuscript\") { \
+              edges { \
+                node { \
+                  body \
+                } \
+              } \
+            } \
+          } \
+        } \
+      } \
+    }"
+  }),
+}
+
+request(options, function (error, response, body) {
+  if (error) {
+    return `ERROR!: ${error}`;
+  } else if (response.statusCode === 200) {
+    console.log('GitHub api was successfully queried 🎉\n');
+    fs.writeFile('data.json', body, (err) => {
+      if (err) throw err;
+      console.log(`data.json was successfully written 🎉\n`);
+    });
+    return;
+  } else {
+    return `Problem! Status code = ${response.statusCode}, response = ${response}`;
+  };
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 Node.js app to get list of NCBI-Hackathons shipped software via GitHub api v4.
+
+##### the big idea
+Automate the ncbi-hackathons.github.io/index.html page automatically via:
+  - auto data-fetch:
+    - a node.js app running on zeit.co/now that queries the GitHub api v4 X times a day
+    - a javascript front end app that, whenever someone goes to `ncbi-hackathons.github.io/`, builds out the site according to the data in the node.js app above
+
+###### details for the node.js app:
+  - get the list of NCBI-Hackathons repos that:
+    - of those repos that have at least 1 release:
+      - for all repos with an issue with the label "Manuscript"
+        - get the body of the issue with the "Manuscript" label
