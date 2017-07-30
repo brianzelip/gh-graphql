a0d1522653c65a0c27ba3c0dc68ce0a67dcb164e
Brian Zelip <bzelip@gmail.com>
2017-07-30 12:30:19 -0400

    clean up index.js after oauth token push

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const fs = require('fs');
 const request = require('request');
 
-// curl -H "Authorization: bearer 159432c7e188822c3440f09b714e0e3c75839ac7" -X POST -d " \
-//  { \
-//    \"query\": \"query {organization(login: \\\"NCBI-Hackathons\\\") {login repositories(first: 100) {totalCount nodes {name description url languages(first: 1) {edges {node {name}}}releases(first: 1) {totalCount}}}}}\"
-//  } \
-// " https:\/\/api.github.com\/graphql > data.json
-
-
-// the Request docs seem to conflate Authorization and Header, although technically Authentication is a request header -- does this fact mean that they are peers, not parent-child?
-
-
 const options = {
   'uri': 'https://api.github.com/graphql',
   'method': 'POST',
